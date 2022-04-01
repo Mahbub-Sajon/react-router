37e273a0d1117fa5d23a5766f653bdc48ce1dc02
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-04-02 01:37:08 +0600

    friendDetail

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './components/About/About';
+import FriendDetail from './components/FriendDetail/FriendDetail';
 import Friends from './components/Friends/Friends';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import NotFound from './components/NotFound/NotFound';
 
@@ -9,10 +11,11 @@ import NotFound from './components/NotFound/NotFound';
 function App() {
   return (
     <div className="App">
-    <h1>Welcome</h1>
+        <Header></Header>
     <Routes>
       <Route path='/' element= {<Home></Home>}></Route>
       <Route path ='/friends' element={<Friends></Friends>}></Route>
+      <Route path='/friend/:friendId' element={<FriendDetail></FriendDetail>}></Route>
       <Route path='/about' element={<About></About>}></Route>
       <Route path='*' element={<NotFound></NotFound>}></Route>
     </Routes>
